cfddc4158d0ccecaea015bcd558694f049b61cbf
stevejamesson <steve@evendev.com>
2018-11-16 14:48:29 -0500

    old site.js data file migrated

---
+++ src/_data/site.js
@@ -0,0 +1,14 @@
+const copyrightDate = new Date().getFullYear()
+
+module.exports = () => ({
+  title: 'Custom Homes of Asheville',
+
+  email: 'steve@chofasheville.com',
+  phone: '828-230-0365',
+
+  socials: {
+    facebook: "http://www.facebook.com/customhomesofasheville",
+  },
+
+  copyrightDate,
+})
